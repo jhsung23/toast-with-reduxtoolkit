e82365430378952a6a7a0ec038d8bcfe306fe44c
sungjihyun <jhsung23@gmail.com>
2023-10-18 22:36:09 +0900

    set: toast 뷰 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 import './App.css';
+import ToastContainer from './components/toast/ToastContainer';
+import useToast from './hooks/useToast';
 
 function App() {
-  return <></>;
+  const toast = useToast();
+
+  const showToast = () => {
+    toast('토스트를 만들었습니다 🍞');
+  };
+
+  return (
+    <>
+      <main className="main">
+        <button className="alert-toast-button" onClick={showToast}>
+          토스트 알림 띄우기
+        </button>
+      </main>
+      <ToastContainer />
+    </>
+  );
 }
 
 export default App;
